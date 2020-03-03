75c37ebef831b6253c507e14fc9f8835bf8b05eb
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-03 11:16:16 -0800

    added footer to all pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, ViewChild } from '@angular/core';
 
 import { Platform, MenuController, Nav } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ListPage } from '../pages/list/list';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 import { SettingsPage } from '../pages/settings/settings';
+import { ProfilePage } from '../pages/profile/profile';
 
 @Component({
   templateUrl: 'app.html'
@@ -40,25 +40,22 @@ export class MyApp {
 
     // set our app's pages
     this.pages = [
-      { title: 'Home', component: HelloIonicPage },
+      { title: 'Locations', component: HelloIonicPage },
       { title: 'Orders', component: ListPage },
+      { title: 'Profile', component: ProfilePage },
       { title: 'Settings', component: SettingsPage },
     ];
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
 
   openPage(page) {
-    // close the menu when clicking a link from the menu
     this.menu.close();
-    // navigate to the new page if it is not the current page
     this.nav.setRoot(page.component);
   }
 
