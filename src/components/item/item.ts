14493d25eca59b32261359f4fe3927d6c15341d6
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-03 15:25:40 -0800

    updating

--- src/components/item/item.ts
+++ src/components/item/item.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from 'ionic-angular';
 
 @Component({
   selector: 'item',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class ItemComponent {
 
+  items: Item[] = []
 
   constructor() {
     console.log('Hello ItemComponent Component');
