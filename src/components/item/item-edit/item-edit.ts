b2b9c29e26a6c2c2c940b5487547b493baab80d7
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-03 15:24:56 -0800

    item-edit

--- src/components/item/item-edit/item-edit.ts
+++ src/components/item/item-edit/item-edit.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
-/**
- * Generated class for the ItemEditComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
 @Component({
   selector: 'item-edit',
   templateUrl: 'item-edit.html'
