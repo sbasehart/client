98ee06a75c6d2893f3a418b1864001e074cf6177
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-03 15:32:14 -0800

    order-edit oninit

--- src/components/order/order-edit/order-edit.ts
+++ src/components/order/order-edit/order-edit.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from '../order.model';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'order-edit',
   templateUrl: 'order-edit.html'
 })
 export class OrderEditComponent implements OnInit{
-  orders: Order[]= []
+  
+  id: number;
+  editMode = false;
+  orderForm: FormGroup;
 
   constructor() {
     console.log('Hello OrderEditComponent Component');
